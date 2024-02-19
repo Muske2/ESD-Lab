de1c71e0cea3f8213ed1b189ff2bd8713b1585aa
Anmol Muskan <98347170+Muske2@users.noreply.github.com>
2024-02-19 15:11:49 +0530

    Update JohnsonCounter.c

--- LAB 5/JohnsonCounter.c
+++ LAB 5/JohnsonCounter.c
@@ -3,20 +3,23 @@
 int main()
 {
 	unsigned int i,j;
-	unsigned long X=0X00000010;
+	
 	LPC_PINCON->PINSEL0&=0XFF0000FF;
 	LPC_GPIO0->FIODIR|=0X00000FF0;
+	while(1){
+		unsigned long X=0X00000010;
 	for(j=0;j<8;j++)
 	{
 		LPC_GPIO0->FIOSET|=X;
-		for(i=0;i<1000;i++);
+		for(i=0;i<100000;i++);
 		X=X<<1;
 	}
 	X=0X00000010;
 	for(j=0;j<8;j++)
 	{
 		LPC_GPIO0->FIOCLR|=X;
-		for(i=0;i<1000;i++);
+		for(i=0;i<100000;i++);
 		X=X<<1;
 	}
+}
 }
